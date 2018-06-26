a465be2244e438df0a0dafd710cac44bacbd21a7
Damien Nivault <damien.nivault@supinternet.fr>
2018-06-26 11:17:04 +0200

    Added form

---
+++ Web/views/auth_form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login fb</title>
+
+<link href="../../web/assets/css/auth_form_css.css" rel="stylesheet" type="text/css"  />
+</head>
+
+<body>
+
+<section class="login-form-wrap">
+  <h1>Sup'Internet connect</h1>
+  <form class="login-form" action="POST" action="#">
+    <label>
+      <input type="email" name="email" class="" required placeholder="Email">
+    </label>
+    <label>
+      <input type="password" name="password" required placeholder="Password">
+    </label>
+    <input type="submit" value="Login">
+  </form>
+  <h5><a href="#">Forgot password</a></h5>
+</section>
+</body>
+
+</html>
