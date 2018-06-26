2d9472dbad489c1e7cf47becb95e07169962f59c
sodaranh <sodara.nhek@supinternet.fr>
2018-06-26 11:24:55 +0200

    route and action generate auth_token

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,4 +9,12 @@ class DefaultController
         echo "TEST";
     }
 
+    function  authTokenAction() {
+        $bytes = random_bytes(255);
+        $token = bin2hex($bytes);
+    
+        $res = array('auth_token'=>$token);
+        //TODO insert token into DB
+        return json_encode($res);
+    }
 }
