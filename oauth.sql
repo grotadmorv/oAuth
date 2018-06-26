c67a09638596aca9b7003b283ab3be6a294442ae
Grotadmorv <apexp@hotmail.fr>
2018-06-26 12:23:10 +0200

    fix bdd value token long

--- oauth.sql
+++ oauth.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `token` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
+  `value` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
