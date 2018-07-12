c40e399b19ba8f3ba0c46a7677b33237a84749a3
Nathael Arki <nathael.arki@supinternet.fr>
2018-07-12 16:41:01 +0200

    te

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -95,7 +95,7 @@ class DefaultController
         if (count($tokens) == 0 || $tokens[0]['type'] != 'confirm') {
             echo json_encode(array(
                 'status' => 'error',
-                'token'   => $tokens[0]
+                'tokenCount'   => count($tokens)
             ));
             return;
         }
